54367f8927041bfc093303eab869aa0f52643586
Leonardo Mezzanotti <leomezza@gmail.com>
2020-10-24 21:56:23 -0300

    Refactored Header.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import './Card.css';
 
+import Header from '../Header/Header';
+
 const Card = ({ imagePath, imageName, cardTitle, description }) => {
   return (
     <div className="card-container">
       <img src={imagePath} alt={imageName}/>
-      <h3>{cardTitle}</h3>
+      <Header type="h3" text={cardTitle} />
+      {/* <h3>{cardTitle}</h3> */}
       <p>{description}</p>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,28 @@
 import React from 'react';
 
 const Header = (props) => {
-  switch (props.type) {
-    case 'h1':
-      return <h1>{props.text}</h1>;
-    case 'h2':
-      return <h2>{props.text}</h2>;
-    case 'h3':
-      return <h3>{props.text}</h3>;
-    case 'h4':
-      return <h4>{props.text}</h4>;
-    case 'h5':
-      return <h5>{props.text}</h5>;
-    case 'h6':
-      return <h6>{props.text}</h6>;
-    default:
-      return <h1>{props.text}</h1>;
+  if (/^h[1-6]$/.test(props.type)) {
+    const HeaderTag = `${props.type}`;
+    return <HeaderTag>{props.text}</HeaderTag>
+  } else {
+    return <h1>{props.text}</h1>;
   }
+  // switch (props.type) {
+  //   case 'h1':
+  //     return <h1>{props.text}</h1>;
+  //   case 'h2':
+  //     return <h2>{props.text}</h2>;
+  //   case 'h3':
+  //     return <h3>{props.text}</h3>;
+  //   case 'h4':
+  //     return <h4>{props.text}</h4>;
+  //   case 'h5':
+  //     return <h5>{props.text}</h5>;
+  //   case 'h6':
+  //     return <h6>{props.text}</h6>;
+  //   default:
+  //     return <h1>{props.text}</h1>;
+  // }
 };
 
 export default Header;
